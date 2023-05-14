72f30e7e3f2ad5d3c5c8df787a809c04d61cacf9
immuhc <sumanthchummisumanth@gmail.com>
2023-05-14 23:19:36 +0530

    2nd Commit

--- src/A.java
+++ src/A.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class A {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		//Good Night
+		//goodmorning
 		System.out.println("enter the values of x and y");
 		int x = sc.nextInt();
 		int y = sc.nextInt();
