740acd3a0f1248a37973da0515c3d8f1585f5e49
immuhc <sumanthchummisumanth@gmail.com>
2023-05-14 20:10:33 +0530

    src added

---
+++ src/A.java
@@ -0,0 +1,17 @@
+//Swapping of 2 nos
+
+import java.util.Scanner;
+
+public class A {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter the values of x and y");
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		System.out.println("before swapping :" + x + "" + y);
+		x = x + y;
+		y = x - y;
+		x = x - y;
+		System.out.println("after swapping :" + x + "" + y);
+	}
+}
