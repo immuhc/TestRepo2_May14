0dfa66175c08d92a21185f3ea85d0db22b22d9e9
immuhc <114220936+immuhc@users.noreply.github.com>
2023-05-14 23:14:51 +0530

    Update A.java

--- src/A.java
+++ src/A.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class A {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		//Good Night
 		System.out.println("enter the values of x and y");
 		int x = sc.nextInt();
 		int y = sc.nextInt();
